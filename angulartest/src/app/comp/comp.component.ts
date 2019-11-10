13a9fbb5a2921ceb57fcf914631373f26f0a5674
hiremathk321 <hiremathk321@gmail.com>
2019-11-10 15:34:15 +0530

    dfgfds

---
+++ angulartest/src/app/comp/comp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comp',
+  templateUrl: './comp.component.html',
+  styleUrls: ['./comp.component.css']
+})
+export class CompComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
